1d55c947ee6d6683e5748182e5859bd683532c0c
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-11-17 22:35:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mvvm/model/DataJenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.mvvm.model
+
+import com.example.mvvm.R
+
+object DataJenisKelamin {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
---
+++ app/src/main/java/com/example/mvvm/ui/View/DetailMahasiswaView.kt
@@ -0,0 +1,8 @@
+package com.example.mvvm.ui.View
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailMahasiswaView(
+    navController: NavHostController
+)
